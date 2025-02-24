0193fcd19b020c794f6ee36f4e40f09d5336a2f1
Mahipat Gami <mahipat.gami@promptdairytech.in>
2025-02-24 11:18:14 +0530

    insert record script

---
+++ insertuser.sql
@@ -0,0 +1,7 @@
+
+Insert into TblUser values ('Parth rethaliya','Parthrethaliya@gmail.com','parth')
+Insert into TblUser values('Vishal gami','Vishalgami@gmail.com','vishal');
+Insert into TblUser values('Vraj patel','vrajpatel@gmail.com','vraj');
+Insert into TblUser values('Milan gandhi','Milangandhi@gmail.com','milan');
+Insert into TblUser values('Sahil savaj','Sahilsavaj@gmail.com','sahil');
+
