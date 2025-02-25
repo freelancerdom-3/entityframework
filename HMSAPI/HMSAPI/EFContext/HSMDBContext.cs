92e84a343ba7c6c0257140619a227275dd2609e1
Nirmal Patel <Nirmal Patel@LAPTOP-0QHQ11A5>
2025-02-25 18:51:00 +0530

    Hospital Type

--- HMSAPI/HMSAPI/EFContext/HSMDBContext.cs
+++ HMSAPI/HMSAPI/EFContext/HSMDBContext.cs
@@ -1,36 +1,41 @@
-﻿using HMSAPI.Model.TblUser;
+﻿using HMSAPI.Model.TblHospitalTyp;
+using HMSAPI.Model.TblUser;
 using Microsoft.EntityFrameworkCore;
 
 namespace HMSAPI.EFContext
 {
-    public class HSMDBContext:DbContext
+    public class HSMDBContext : DbContext
     {
-        public HSMDBContext(DbContextOptions<HSMDBContext> option):base(option)
+
+        public HSMDBContext(DbContextOptions<HSMDBContext> option) : base(option)
         {
-                
+
         }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //connection related changes 
-            //prepare .json file, add connection string
-            //Connection string : servername, dbname, authentication type, secure,
-            // register here,
+
             var configdata = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json",optional:false)
+                .AddJsonFile("appsettings.json", optional: false)
                 .Build();
             string? DFConnection = configdata.GetConnectionString("DefaultConnection");
             optionsBuilder.UseSqlServer(DFConnection);
             base.OnConfiguring(optionsBuilder);
         }
 
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<TblUserModel>().ToTable("TblUser");
-            base.OnModelCreating(modelBuilder); 
-
+            modelBuilder.Entity<TblHospitalTypModel>().ToTable("TblHospitalTyp");
+            base.OnModelCreating(modelBuilder);
         }
+
+        public DbSet<TblHospitalTypModel> TblHospitalTypes { get; set; }
 
         public DbSet<TblUserModel> TblUsers { get; set; }
     }
 }
+
