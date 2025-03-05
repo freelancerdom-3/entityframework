2744084ea110ea789fa66cb6f8004e9fd085dd10
Parth Rethaliya <parthrethaliya6105@gmail.com>
2025-03-05 16:37:53 +0530

    IN SQL HOSPITAL DEPARTMENT TABLE CHANGE IN COLUMNAME AND TABLE NAME

--- SQL/HOSPITAL DEPT.sql
+++ SQL/HOSPITAL DEPT.sql
@@ -1,28 +1,30 @@
 USE [HSMDB]
 GO
-/****** Object:  Table [dbo].[TblHospitalDept]    Script Date: 2/27/2025 10:26:17 AM ******/
+/****** Object:  Table [dbo].[TbLHospitalDepartment]    Script Date: 3/5/2025 4:32:10 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[TblHospitalDept](
-	[DeptId] [int] IDENTITY(1,1) NOT NULL,
-	[Department] [varchar](100) NULL,
+CREATE TABLE [dbo].[TbLHospitalDepartment](
+	[HospitalDepartmentID] [int] IDENTITY(1,1) NOT NULL,
+	[DepartmentName] [varchar](100) NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[DeptId] ASC
+	[HospitalDepartmentID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-SET IDENTITY_INSERT [dbo].[TblHospitalDept] ON 
+SET IDENTITY_INSERT [dbo].[TbLHospitalDepartment] ON 
 GO
-INSERT [dbo].[TblHospitalDept] ([DeptId], [Department]) VALUES (1, N'Emergency Department')
+INSERT [dbo].[TbLHospitalDepartment] ([HospitalDepartmentID], [DepartmentName]) VALUES (1, N'Emergency Department')
 GO
-INSERT [dbo].[TblHospitalDept] ([DeptId], [Department]) VALUES (3, N'Cardiology Department')
+INSERT [dbo].[TbLHospitalDepartment] ([HospitalDepartmentID], [DepartmentName]) VALUES (3, N'Cancer Department')
 GO
-INSERT [dbo].[TblHospitalDept] ([DeptId], [Department]) VALUES (4, N'Orthopedic Department')
+INSERT [dbo].[TbLHospitalDepartment] ([HospitalDepartmentID], [DepartmentName]) VALUES (4, N'Orthopedic Department')
 GO
-INSERT [dbo].[TblHospitalDept] ([DeptId], [Department]) VALUES (5, N'Pediatrics Department')
+INSERT [dbo].[TbLHospitalDepartment] ([HospitalDepartmentID], [DepartmentName]) VALUES (5, N'Pediatrics Department')
 GO
-SET IDENTITY_INSERT [dbo].[TblHospitalDept] OFF
+INSERT [dbo].[TbLHospitalDepartment] ([HospitalDepartmentID], [DepartmentName]) VALUES (8, N'Gynec Department')
+GO
+SET IDENTITY_INSERT [dbo].[TbLHospitalDepartment] OFF
 GO
