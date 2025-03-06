a6558e8184c1a4c65d7865366431b2b74d549805
Nirmal Patel <Nirmal Patel@LAPTOP-0QHQ11A5>
2025-03-06 10:23:20 +0530

    changes in GetAll  Method  ( responsemodel)

--- HMSAPI/HMSAPI/Service/TblUser/TblUser.cs
+++ HMSAPI/HMSAPI/Service/TblUser/TblUser.cs
@@ -71,7 +71,7 @@ namespace HMSAPI.Service.TblUser
                     lstUsers = connection.GetTblUserViewModel.FromSqlRaw($@"SELECT tuser.*,trole.rolename 
                     FROM [HSMDB].[dbo].[TblUser] tuser
                     inner join tblrole trole on trole.roleid=tuser.roleid where fullname like '%{searchBy}%'").ToList();
-                    responseModel.Data = true;
+                    responseModel.Data = lstUsers;
                     responseModel.StatusCode = HttpStatusCode.OK;
                     responseModel.Message = "Successfully";
                 }
