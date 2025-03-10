438ba00eca83a61849f10ffc98818caaecd5ebe0
Nirmal Patel <Nirmal Patel@LAPTOP-0QHQ11A5>
2025-03-10 18:21:21 +0530

    Feedback Viewmodel

---
+++ HMSAPI/HMSAPI/Model/TblFeedback/ViewModel/GetTblFeedbackViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HMSAPI.Model.TblFeedback.ViewModel
+{
+    public class GetTblFeedbackViewModel
+    {
+        [Key]
+        public int FeedbackId { get; set; }
+        public  string? FullName {  get; set; }
+        public string? Comments { get; set; }
+        public int  Rating  { get; set; }
+        public DateOnly FeedbackDate {  get; set; } 
+
+    }
+}
