ebc4a9ed9ef951c7f17e2dbb2398dd5bac10474b
admin <admin@SAHILSAVAJ>
2025-03-10 18:33:22 +0530

    Add RoomTypeFacilityModel

---
+++ HMSAPI/HMSAPI/Model/TblRoomTypeFacilityMapping/TblRoomTypeFacilityMappingModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HMSAPI.Model.TblRoomTypeFacilityMapping
+{
+    public class TblRoomTypeFacilityMappingModel
+    {
+        [Key]
+        public int RoomTypeFacilityMappingID { get; set; }
+        public int RoomID { get; set; }
+        public int FacilityID { get; set; }
+    }
+}
