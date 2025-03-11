5632268114f42961151c502688e90325d7be9e8b
stava <stava@STAVAN>
2025-03-11 18:12:04 +0530

    Add SecurityModel

---
+++ HMSAPI/HMSAPI/Model/GenericModel/SecurityModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HMSAPI.Model.GenericModel
+{
+    public class SecurityModel
+    {
+        public int? CreateBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int? UpdateBy { get; set; }
+        public DateTime? UpdateOn { get; set; }
+        public bool? IsActive { get; set; }
+        public int VersionNo { get; set; }
+        public int IncreamentVersion()
+        {
+            return VersionNo += 1;
+        }
+    }
+}
