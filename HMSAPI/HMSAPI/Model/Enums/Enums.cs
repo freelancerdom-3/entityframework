a5e67af1342f968ec80ccf578b39196c7ac43483
rajeshgami <rmgami93@gmail.com>
2025-03-19 13:00:29 +0530

    create Enum

---
+++ HMSAPI/HMSAPI/Model/Enums/Enums.cs
@@ -0,0 +1,39 @@
+﻿namespace HMSAPI.Model.Enums
+{
+    public class Enums
+    {
+        public enum Roles
+        {
+            Admin = 1,
+            Patient = 2,
+        }
+
+        public enum Menus
+        {
+            Master = 1,
+            Setting = 2,
+            Patients = 3,
+            Admin = 4,
+            Users = 5,
+            HospitalType = 6,
+            HospitalDepartment = 7,
+            Role = 8,
+            Shift = 9,
+            DiseaseType = 10,
+            MedicineType = 11,
+            RoomType = 12,
+            FacilityType = 13,
+            Employeeshift = 14,
+            Room = 15,
+            RoomLocations = 16,
+            RoomFacility = 17,
+            EmployeeDepartment = 18,
+            PatientData = 19,
+            TreatmentDetails = 20,
+            MedicineDetails = 21,
+            AdmissionDetails = 22,
+            Billing = 23
+
+        }
+    }
+}
