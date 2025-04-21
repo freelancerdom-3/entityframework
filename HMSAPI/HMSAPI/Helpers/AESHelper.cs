7029a88fb5d18708bfd8b52bc44a655ecd32bb4b
kulas <kulas@LAPTOP-BSDP7EEM>
2025-04-21 15:56:53 +0530

    helper

---
+++ HMSAPI/HMSAPI/Helpers/AESHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+public static class AESHelper
+{
+    private static readonly string key = "12345678901234567890123456789012"; // 32-char
+    private static readonly string iv = "1234567890123456"; // 16-char
+
+    public static string Encrypt(string plainText)
+    {
+        using Aes aesAlg = Aes.Create();
+        aesAlg.Key = Encoding.UTF8.GetBytes(key);
+        aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+        aesAlg.Mode = CipherMode.CBC;
+        aesAlg.Padding = PaddingMode.PKCS7;
+
+        ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+        using MemoryStream msEncrypt = new();
+        using (CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write))
+        using (StreamWriter swEncrypt = new(csEncrypt))
+            swEncrypt.Write(plainText);
+
+        return Convert.ToBase64String(msEncrypt.ToArray());
+    }
+}
